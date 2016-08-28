c6bfbc03cc2c3f54fc7f1e6ddca31f07a33b698f
Weifeng Hu <weifeng.hu.288@gmail.com>
2016-08-28 14:24:07 -0700

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   test_stoch_basis/main.cpp
    
     Untracked files:
    	main.o
    	stoch_driver
    	test_stoch_basis/.nfs0000000000ac02d800000168
    	test_stoch_basis/.nfs0000000000ac03630000016d
    	test_stoch_basis/main.o
    	test_stoch_basis/out
    	test_stoch_basis/stoch_eigen_test
    	test_stoch_basis/symdiag.o
    	test_unity_matrix/averaged_distance_1000
    	test_unity_matrix/averaged_distance_2000
    	test_unity_matrix/averaged_distance_3000
    	test_unity_matrix/averaged_distance_4000
    	test_unity_matrix/averaged_distance_5000
    	test_unity_matrix/averaged_distance_6000
    	test_unity_matrix/averaged_distance_7000
    	test_unity_matrix/averaged_distance_8000
    	test_unity_matrix/averaged_distance_9000
    	test_unity_matrix/dim_of_basis_1000
    	test_unity_matrix/dim_of_basis_2000
    	test_unity_matrix/dim_of_basis_3000
    	test_unity_matrix/dim_of_basis_4000
    	test_unity_matrix/dim_of_basis_5000
    	test_unity_matrix/dim_of_basis_6000
    	test_unity_matrix/dim_of_basis_7000
    	test_unity_matrix/dim_of_basis_8000
    	test_unity_matrix/dim_of_basis_9000

--- mat_stoch_diag/test_stoch_basis/main.cpp
+++ mat_stoch_diag/test_stoch_basis/main.cpp
@@ -77,6 +77,7 @@ int main( int argc, char* argv[] ) {
   }
   std :: cout << "done" << std :: endl;
 
+
   // Now take a small portion of the new matrix
   std :: cout << "Obtaining eigenvalues of a sub matrix of "
               << non_residual_space_size << " x " << non_residual_space_size << std :: endl;
@@ -107,11 +108,11 @@ int main( int argc, char* argv[] ) {
   std :: cout << "Obtaining eigenvalues of a stochastically transformed "
               << target_space_size << " x " << target_space_size << std :: endl;
   mat_stoch_diag :: StochasticBasisMixer mixer_pure_stoch( target_space_size );
-  mat_stoch_diag :: StochasticSpace target_space_pure_stoch 
+  mat_stoch_diag :: StochasticSpace target_space_pure_stoch
     = mixer_pure_stoch.equal_prob_residual_space( eigen_pair, non_residual_space_size );
   mat_stoch_diag :: StochasticTransformer transformer_pure_stoch( &original_space, &target_space_pure_stoch );
   mat_stoch_diag :: SimpleMatrix target_matrix_pure_stoch
-   = transformer_pure_stoch.transform_by_rotation_matrix( new_matrix, target_space_size );
+    = transformer_pure_stoch.transform_by_rotation_matrix( new_matrix, target_space_size );
   eigen_pair_type eigen_pair_pure_stoch = eigen_processor.diagonalise( target_matrix_pure_stoch );
 
 
@@ -135,7 +136,7 @@ int main( int argc, char* argv[] ) {
 
   mat_stoch_diag :: SimpleMatrix target_matrix_mixed
     = transformer_mixed.transform_by_rotation_matrix( new_matrix, target_space_size );
-  eigen_pair_type eigen_pair_mixed = eigen_processor.diagonalise( target_matrix_mixed  );
+  eigen_pair_type eigen_pair_mixed = eigen_processor.diagonalise( target_matrix_mixed );
 
   std :: cout << "original\t\thalf\t\thalf_p\t\tstoch\t\thalf_st" << std :: endl;
   std :: cout << eigen_pair.second.size() << "\t\t" 
@@ -144,18 +145,18 @@ int main( int argc, char* argv[] ) {
               << eigen_pair_pure_stoch.second.size() << "\t\t" 
               << eigen_pair_mixed.second.size() << std :: endl;
   for( size_t i = 0; i < non_residual_space_size; i++ ) {
-    std :: cout << std :: fixed << eigen_pair.second.at(i)              << "\t\t" 
-                                << eigen_pair_non_residual.second.at(i) << "\t\t" 
-                                << eigen_pair_sub_plus.second.at(i)     << "\t\t" 
-                                << eigen_pair_pure_stoch.second.at(i)   << "\t\t" 
-                                << eigen_pair_mixed.second.at(i)        
+    std :: cout << std :: fixed << eigen_pair.second.at(i)              << "\t\t"
+                                << eigen_pair_non_residual.second.at(i) << "\t\t"
+                                << eigen_pair_sub_plus.second.at(i)     << "\t\t"
+                                << eigen_pair_pure_stoch.second.at(i)   << "\t\t"
+                                << eigen_pair_mixed.second.at(i)        << "\t\t"
                                 << std :: endl;
   }
 
   return 0;
+
 //  eigen_pair.first *= sqrt(double(dimension_of_basis));
 //  eigen_pair.first.print();
-
 
   mat_stoch_diag :: StochasticUnityOperator operator_matrix_original( &original_space );
   double distance_from_unity_original = operator_matrix_original.distance_from_pure_unity_matrix();
@@ -163,8 +164,8 @@ int main( int argc, char* argv[] ) {
                = distance_from_unity_original/( dimension_of_basis * dimension_of_basis );
 //  std :: cout << "dim of space: " << dimension_of_basis << std :: endl;
 //  std :: cout << "dim of basis: " << dimension_of_space << std :: endl;
-  std :: cout << "total distance from unity matrix[original space]: " << distance_from_unity_original << std :: endl;
-  std :: cout << "averaged distance from unity matrix[original space]: " << averaged_distance_from_unity_original << std :: endl;
+  std :: cout << "total distance from unity matrix [original space]: "    << distance_from_unity_original          << std :: endl;
+  std :: cout << "averaged distance from unity matrix [original space]: " << averaged_distance_from_unity_original << std :: endl;
   std :: cout << " -------------------------------------- " << std :: endl;
 
   mat_stoch_diag :: StochasticBasisMixer basis_mixer( dimension_of_basis );
@@ -182,9 +183,6 @@ int main( int argc, char* argv[] ) {
 
   mat_stoch_diag :: StochasticTransformer transformer( &original_space, &new_space );
   mat_stoch_diag :: SimpleMatrix transformed_matrix = transformer.transform_by_rotation_matrix( new_matrix, target_space_size );
-
-  new_matrix.print();
-  transformed_matrix.print();
 
   std :: pair< mat_stoch_diag :: SimpleMatrix, std :: vector< double > > eigen_pair_x = 
      eigen_processor.diagonalise( transformed_matrix );
