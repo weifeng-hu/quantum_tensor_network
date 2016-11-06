656de2972087503df7c9fa9e0857660f123be3d1
Weifeng Hu <hulakui@gmail.com>
2016-11-06 14:20:06 -0600

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   mat_stoch_diag/general_eigenvalue.f
    
     Untracked files:
    	mat_stoch_diag/main.o
    	mat_stoch_diag/stoch_driver
    	mat_stoch_diag/test_stoch_basis/main.arm.as
    	mat_stoch_diag/test_stoch_basis/main.o
    	mat_stoch_diag/test_stoch_basis/stoch_eigen_test
    	mat_stoch_diag/test_stoch_basis/symdiag.o
    	mat_stoch_diag/test_stoch_diag/main.o
    	mat_stoch_diag/test_stoch_diag/stoch_mat_diag
    	mat_stoch_diag/test_stoch_diag/symdiag.o
    	mat_stoch_diag/test_stoch_space_diag/Makefile
    	mat_stoch_diag/test_stoch_space_diag/general_eigenvalue.o
    	mat_stoch_diag/test_stoch_space_diag/main.o
    	mat_stoch_diag/test_stoch_space_diag/stoch_mat_diag
    	mat_stoch_diag/test_stoch_space_diag/symdiag.o
    	mat_stoch_diag/test_stoch_space_diag/syminverse.o
    	mat_stoch_diag/test_unity_matrix/averaged_distance_1000
    	mat_stoch_diag/test_unity_matrix/averaged_distance_2000
    	mat_stoch_diag/test_unity_matrix/averaged_distance_3000
    	mat_stoch_diag/test_unity_matrix/averaged_distance_4000
    	mat_stoch_diag/test_unity_matrix/averaged_distance_5000
    	mat_stoch_diag/test_unity_matrix/averaged_distance_6000
    	mat_stoch_diag/test_unity_matrix/averaged_distance_7000
    	mat_stoch_diag/test_unity_matrix/averaged_distance_8000
    	mat_stoch_diag/test_unity_matrix/averaged_distance_9000
    	mat_stoch_diag/test_unity_matrix/dim_of_basis_1000
    	mat_stoch_diag/test_unity_matrix/dim_of_basis_2000
    	mat_stoch_diag/test_unity_matrix/dim_of_basis_3000
    	mat_stoch_diag/test_unity_matrix/dim_of_basis_4000
    	mat_stoch_diag/test_unity_matrix/dim_of_basis_5000
    	mat_stoch_diag/test_unity_matrix/dim_of_basis_6000
    	mat_stoch_diag/test_unity_matrix/dim_of_basis_7000
    	mat_stoch_diag/test_unity_matrix/dim_of_basis_8000
    	mat_stoch_diag/test_unity_matrix/dim_of_basis_9000

--- mat_stoch_diag/general_eigenvalue.f
+++ mat_stoch_diag/general_eigenvalue.f
@@ -20,34 +20,56 @@
 
       character*1 job
       character*1 compz
-      double precision, allocatable :: h( :, : )
+      double precision, allocatable :: h(:,:)
       integer ldh, ldz
       double precision, allocatable ::  wr(:)
       double precision, allocatable ::  wi(:)
       double precision, allocatable ::  w(:)
-      double precision, allocatable ::  z(:, : )
+      double precision, allocatable ::  z(:,:)
+
+      double precision, allocatable :: scal(:)
 
       n = min( nrow, ncol )
-      ilo = 1
-      ihi = n
+!      ilo = 1
+!      ihi = n
       lda = n
       lwork = n
       allocate( a( lda, n ) )
       allocate( work( lwork ) )
-      allocate( tau ( n - 1 ) )
+      allocate( tau ( n ) )
+      allocate( scal( n ) )
 
       do i = 1, n
         do j = 1, n
-          a( i, j ) = matrix( i, j )
-        end do
+          a(i,j) = 0.0d0
+        enddo
+      enddo 
+
+      do i = 1, n
+        a(i, i) = 3.0d0
+      end do
+
+      do i = 1, n - 1
+        a(i, i-1) = 2.0d0
+        a(i-1, i) = 2.0d0
       end do
 
       write(*,*)a
+      job = 'P'
+      call dgebal(job, n, a, lda, ilo, ihi, scal, info)
+      write(*,*)a
+      write(*,*)scal
+      write(*,*)ilo
+      write(*,*)ihi
       call dgehrd(n, ilo, ihi, a, lda, tau, work, lwork, info)
+      write(*,*)a
 
       call dorghr(n, ilo, ihi, a, lda, tau, work, lwork, info)
-
+      stop
       write(*,*)a
+      write(*,*)tau
+
+      stop
 
       job = 'E'
       compz = 'N'
