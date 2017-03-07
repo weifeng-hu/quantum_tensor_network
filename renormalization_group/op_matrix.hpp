fe956ca38724d2ad222ca2712c59ff56d29e2940
Weifeng Hu <hulakui@gmail.com>
2017-03-07 00:07:49 -0800

    On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   op_matrix.hpp
    
     Untracked files:
    	../mat_stoch_diag/main.o
    	../mat_stoch_diag/stoch_driver
    	../mat_stoch_diag/test_stoch_basis/main.arm.as
    	../mat_stoch_diag/test_stoch_basis/main.o
    	../mat_stoch_diag/test_stoch_basis/stoch_eigen_test
    	../mat_stoch_diag/test_stoch_basis/symdiag.o
    	../mat_stoch_diag/test_stoch_diag/main.o
    	../mat_stoch_diag/test_stoch_diag/stoch_mat_diag
    	../mat_stoch_diag/test_stoch_diag/symdiag.o
    	../mat_stoch_diag/test_stoch_space_diag/general_eigenvalue.o
    	../mat_stoch_diag/test_stoch_space_diag/main.o
    	../mat_stoch_diag/test_stoch_space_diag/stoch_mat_diag
    	../mat_stoch_diag/test_stoch_space_diag/symdiag.o
    	../mat_stoch_diag/test_stoch_space_diag/syminverse.o
    	../mat_stoch_diag/test_unity_matrix/averaged_distance_1000
    	../mat_stoch_diag/test_unity_matrix/averaged_distance_2000
    	../mat_stoch_diag/test_unity_matrix/averaged_distance_3000
    	../mat_stoch_diag/test_unity_matrix/averaged_distance_4000
    	../mat_stoch_diag/test_unity_matrix/averaged_distance_5000
    	../mat_stoch_diag/test_unity_matrix/averaged_distance_6000
    	../mat_stoch_diag/test_unity_matrix/averaged_distance_7000
    	../mat_stoch_diag/test_unity_matrix/averaged_distance_8000
    	../mat_stoch_diag/test_unity_matrix/averaged_distance_9000
    	../mat_stoch_diag/test_unity_matrix/dim_of_basis_1000
    	../mat_stoch_diag/test_unity_matrix/dim_of_basis_2000
    	../mat_stoch_diag/test_unity_matrix/dim_of_basis_3000
    	../mat_stoch_diag/test_unity_matrix/dim_of_basis_4000
    	../mat_stoch_diag/test_unity_matrix/dim_of_basis_5000
    	../mat_stoch_diag/test_unity_matrix/dim_of_basis_6000
    	../mat_stoch_diag/test_unity_matrix/dim_of_basis_7000
    	../mat_stoch_diag/test_unity_matrix/dim_of_basis_8000
    	../mat_stoch_diag/test_unity_matrix/dim_of_basis_9000
    	test_operators/

--- renormalization_group/op_matrix.hpp
+++ renormalization_group/op_matrix.hpp
@@ -5,6 +5,7 @@
 #include <utility>
 #include <iostream>
 #include "../mat_stoch_diag/simple_matrix.hpp"
+#include "../mat_stoch_diag/matrix_operations.hpp"
 #include "./sub_space.hpp"
 
 namespace renormalization_group {
@@ -196,6 +197,30 @@ public:
 
   // Matrix operations, *, +, x
 public:
+  this_type& operator*= ( const double& coeff ) {
+    for( size_t i_qn = 0; i_qn < n_qn_row(); i_qn++ ) {
+      for( size_t j_qn = 0; j_qn < n_qn_col(); j_qn++ ) {
+        if( (*this)(i_qn, j_qn).second.ncol() != 0 ) {
+          (*this)(i_qn, j_qn).second *= coeff;
+        }
+      }
+    }
+    return *this;
+  } // end of operator*=
+
+  friend
+    this_type operator* ( const double& coeff, const this_type& op_mat ) {
+      this_type retval = op_mat;
+      op_mat *= coeff;
+      return retval;
+    } // end of operator*
+
+  friend
+    this_type operator* ( const this_type& op_mat, const double& coeff ) {
+      this_type retval = coeff * op_mat;
+      return retval;
+    } // end of operator*
+
   friend
     this_type operator* ( this_type& op_a, this_type& op_b ) {
 
@@ -241,7 +266,120 @@ public:
       } // end of for i_qn
 
       return retval;
+    } // end of operator* for matrix * matrix 
+
+  friend
+    this_type operator+ ( this_type& op_a, this_type& op_b ) {
+      this_type retval;
+      if( op_a.n_qn_row() != op_b.n_qn_row() ) {
+        std :: cout << "error: two operator matrices of the same site cannot add" << std :: endl;
+        std :: cout << "n_qn_row of A != n_qn_row of B" << std :: endl;
+        std :: cout << op_a.n_qn_row() << " " << op_b.n_qn_row() << std :: endl;
+        abort();
+      }
+      if( op_a.n_qn_col() != op_b.n_qn_col() ) {
+        std :: cout << "error: two operator matrices of the same site cannot add" << std :: endl;
+        std :: cout << "n_qn_col of A != n_qn_col of B" << std :: endl;
+        std :: cout << op_a.n_qn_col() << " " << op_b.n_qn_col() << std :: endl;
+        abort();
+      }
+
+      retval.resize( op_a.n_qn_row(), op_a.n_qn_col() );
+
+      for( size_t i_qn = 0; i_qn < op_a.n_qn_row(); i_qn++ ) {
+        for( size_t j_qn = 0; j_qn < op_a.n_qn_col(); j_qn++ ) {
+          const space_type qn_i     = op_a( i_qn, j_qn ).first.first;
+          const space_type qn_i_ref = op_b( i_qn, j_qn ).first.first;
+          if( qn_i != qn_i_ref | ( qn_i.dim() != qn_i_ref.dim() ) ) {
+            std :: cout << "qn_i != qn_i_ref in same_site_plus() " << std :: endl;
+            abort();
+          }
+
+          const space_type qn_j     = op_a( i_qn, j_qn ).first.second;
+          const space_type qn_j_ref = op_b( i_qn, j_qn ).first.second;
+          if( qn_j != qn_j_ref | ( qn_j.dim() != qn_j_ref.dim() ) ) {
+            std :: cout << "qn_j != qn_j_ref in same_site_plus() " << std :: endl;
+            abort();
+          }
+
+          matrix_type new_matrix;
+          if( op_a.matrix( i_qn, j_qn ).nrow() == 0 ) {
+            new_matrix = op_b.matrix( i_qn, j_qn );
+          } else if( op_b.matrix( i_qn, j_qn ).nrow() == 0 ) {
+            new_matrix = op_a.matrix( i_qn, j_qn );
+          } else if( op_a.matrix( i_qn, j_qn ).nrow() != 0 & op_b.matrix( i_qn, j_qn ).nrow() != 0 ) {
+            new_matrix = op_a.matrix( i_qn, j_qn ) + op_b.matrix( i_qn, j_qn );
+          }
+          retval( i_qn, j_qn ) = 
+            std :: pair< std :: pair< space_type, space_type>, matrix_type > ( std :: pair< space_type, space_type> ( qn_i, qn_j ), new_matrix );
+          
+        }
+      }
+
+      return retval;
+
+    } // end of operator+ of matrix + matrix 
+
+    friend
+      this_type operator^ ( this_type& op_a, this_type& op_b ) {
+
+    this_type op_c;
+
+    int n_qn_row_a = op_a.n_qn_row();
+    int n_qn_col_a = op_a.n_qn_col();
+    int n_qn_row_b = op_b.n_qn_row();
+    int n_qn_col_b = op_b.n_qn_col();
+
+    int n_qn_row_c = n_qn_row_a * n_qn_row_b;
+    int n_qn_col_c = n_qn_col_a * n_qn_col_b;
+
+    op_c.resize( n_qn_row_c, n_qn_col_c );
+
+    for( size_t i = 0; i < n_qn_row_b; i++ ) {
+      for( size_t j = 0; j < n_qn_col_b; j++ ) {
+
+        space_type qn_i_second = op_b.set_qn_pair( i, j ).first;
+        space_type qn_j_second = op_b.set_qn_pair( i, j ).second;
+        matrix_type mat_ij = op_b( i, j ).second;
+
+        for( size_t k = 0; k < n_qn_row_a; k++ ) {
+          for( size_t l = 0; l < n_qn_col_a; l++ ) {
+
+            space_type qn_k_a = op_a.set_qn_pair( k, l ).first;
+            space_type qn_l_a = op_a.set_qn_pair( k, l ).second;
+            matrix_type mat_kl = op_a( k, l ).second;
+
+            space_type qn_row = qn_i_b + qn_k_a;
+            space_type qn_col = qn_j_b + qn_l_a;
+            qn_row.set_dim() = qn_i_b.dim() * qn_k_a.dim();
+            qn_col.set_dim() = qn_j_b.dim() * qn_l_a.dim();
+
+            matrix_type mat_ij_kl;
+            if( mat_ij.nrow() != 0 & mat_kl.nrow() != 0 ) {
+              int nrow_1 = mat_ij.nrow();
+              int ncol_1 = mat_ij.ncol();
+              int nrow_2 = mat_kl.nrow();
+              int ncol_2 = mat_kl.ncol();
+              mat_ij_kl.resize( nrow_1 * nrow_2, ncol_1 * ncol_2 );
+              for( size_t irow = 0; irow < nrow_1; irow++ ) {
+                for( size_t icol = 0; icol < ncol_1; icol++ ) {
+                  for( size_t jrow = 0; jrow < nrow_2; jrow++ ) {
+                    for( size_t jcol = 0; jcol < ncol_2; jcol++ ) {
+                      mat_ij_kl( irow * nrow_1 + jrow, icol * ncol_1 + jcol ) = mat_ij( irow, icol ) * mat_kl( jrow, jcol );
+                    }
+                  }
+                }
+              }
+            }
+            op_c( i * n_qn_row_a + k, j * n_qn_col_a + l ) = std :: make_pair( std :: make_pair( qn_row, qn_col ), mat_ij_kl );
+          }
+        }
+      }
     }
+
+    return op_c;
+
+      } // end of operator^ matrix x matrix, tensor product
 
 public:
   // Accessors & modifiers
